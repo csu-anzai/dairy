3b275b2b5b69efc13f8178c3aab10ff4fe7f9fe2
Ashish Kumar <ashish.kumar2@rsystems.com>
2019-06-28 12:02:12 +0530

    associations updated

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,9 +1,10 @@
 class Batch < ApplicationRecord
   belongs_to :vendor, class_name: 'User', foreign_key: 'user_id', inverse_of: :batches
   belongs_to :item_variant, inverse_of: :batches
-  has_one :item, through: :item_variant
   has_one :stock, inverse_of: :batch, dependent: :destroy
-  # has_one :item_category, through: :item_variant
-  # has_many :item_attributes, through: :item_category
-  # has_many :attribute_choices, through: :item_attributes
+
+  has_one :item, through: :item_variant
+  has_one :item_category, through: :item_variant
+  has_many :item_attributes, through: :item_variant
+  has_many :attribute_choices, through: :item_variant
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,10 @@
 class Stock < ApplicationRecord
   belongs_to :batch, inverse_of: :stock
   belongs_to :unit, inverse_of: :stocks
+
+  has_one :item_variant, through: :batch
+  has_one :item, through: :item_variant
+  has_one :item_category, through: :item_variant
+  has_many :item_attributes, through: :item_variant
+  has_many :attribute_choices, through: :item_variant
 end
