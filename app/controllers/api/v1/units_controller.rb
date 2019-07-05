5fe36c82b4362821f17f855ec48d0b36bfcc3314
Sanjay Yadav <sanjay.yadav1@rsystems.com>
2019-07-05 11:25:27 +0530

    Implemented API for all items and product models.

---
+++ app/controllers/api/v1/units_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::UnitsController < ApplicationController
+
+	def create
+	    unit = Unit.create(attr_choice_params)
+	    render json: unit
+	end
+
+	def index
+		render json: Unit.all
+	end
+
+	def show
+	end
+
+	def update
+		unit = Unit.find(params[:id])
+	    unit.update_attributes(attr_choice_params)
+	    render json: unit
+	end
+
+	def destroy
+		Unit.destroy(params[:id])
+	end
+
+	private
+
+	def attr_choice_params
+		params.require(:unit).permit(:id, :name)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
      resources :item_categories, only: [:create, :index, :show, :update, :destroy]
+     resources :item_attributes, only: [:create, :index, :show, :update, :destroy]
+     resources :attribute_choices, only: [:create, :index, :show, :update, :destroy]
+     resources :items, only: [:create, :index, :show, :update, :destroy]
+     resources :item_variants, only: [:create, :index, :show, :update, :destroy]
+     resources :units, only: [:create, :index, :show, :update, :destroy]
     end 
   end 
 end
