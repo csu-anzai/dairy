f1525f28ca27169f0e9d647aad2cd9221dffe3c5
Sanjay Yadav <sanjay.yadav1@rsystems.com>
2019-09-02 19:03:33 +0530

    Solved Authentication issue and page request method changed.

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PaymentsController < ApplicationController
 	before_action :authorize_request
 
 	def payment_history
-		subs_id = params[:payment][:subscription_id]
+		subs_id = params[:subscription_id]
 		@payments = @current_user.subscriptions.find(subs_id).payments
 		render json: {access_code: I18n.t('access_code.failure')}, status: :unprcessable_entity unless @payments.present?
 	end
